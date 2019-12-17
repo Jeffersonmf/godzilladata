72e6d65a12fc053a8a6a13254a8cd4e5cbbcf54a
Jefferson Marchetti <jefferson@contaswap.com>
2019-12-17 17:01:32 -0300

    Change the data Structure of Json to HDFS Parquet on S3

--- spark/legacy-data-project/src/main/scala/core/EnrichmentEngine.scala
+++ spark/legacy-data-project/src/main/scala/core/EnrichmentEngine.scala
@@ -11,6 +11,8 @@ import utils.Utils
 object EnrichmentEngine {
 
   val spark = sparkContextInitialize()
+  this.spark.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", Environment.aws_access_key())
+  this.spark.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", Environment.aws_secret_key())
 
   private def sparkContextInitialize(): SparkSession = {
     SparkSession.builder
@@ -41,60 +43,36 @@ object EnrichmentEngine {
   private def processInAWS(path: String): Boolean = {
 
     var processStatus: Boolean = false
-    //TODO: Include here all logic to process directly on AWS S3...
-    spark.sparkContext.hadoopConfiguration.set("fs.s3n.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
-    spark.sparkContext.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", Environment.aws_access_key())
-    spark.sparkContext.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey", Environment.aws_access_key())
 
     val sourcePath = Environment.get_AWS_JsonSourceFolder().concat(if (path != null) path else "")
     val destPath = Environment.get_AWS_ParquetDestinationFolder()
 
     val enrichmentFiles = Utils.getListOfFiles(sourcePath)
 
-//      val bucketName = "swap-bigdata"
-//
-//      //file to upload
-//      val fileToUpload = new File("/home/neelkanth/Desktop/Neelkanth_Sachdeva.png")
-//
-//      val yourAWSCredentials = new BasicAWSCredentials(Environment.aws_access_key(), Environment.aws_secret_key())
-//      val amazonS3Client = new AmazonS3Client(yourAWSCredentials)
-//      // This will create a bucket for storage
-//      amazonS3Client.putObject(bucketName, "FileName", fileToUpload)
-
       try {
       if (!Utils.isSourceFolderEmpty(sourcePath)) {
-        val df = spark.read.format("json").json(sourcePath)
-//        spark.read.parquet("s3n://bucket/abc.parquet")    //Read
-//        df.rdd.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", Environment.aws_access_key())
-//        df.rdd.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", Environment.aws_secret_key())
-//        df.rdd.sparkContext.hadoopConfiguration.set("fs.s3.access.key", Environment.aws_access_key())
-//        df.rdd.sparkContext.hadoopConfiguration.set("fs.s3.secret.key", Environment.aws_secret_key())
-//        df.rdd.sparkContext.hadoopConfiguration.set("fs.s3n.access.key", Environment.aws_access_key())
-//        df.rdd.sparkContext.hadoopConfiguration.set("fs.s3n.secret.key", Environment.aws_secret_key())
-//        spark.sparkContext.hadoopConfiguration.set("fs.s3a.endpoint", "s3.ap-south-1.amazonaws.com")
-        spark.sparkContext.hadoopConfiguration.set("fs.s3a.awsAccessKeyId", Environment.aws_access_key())
-        spark.sparkContext.hadoopConfiguration.set("fs.s3a.awsSecretAccessKey", Environment.aws_secret_key())
-        df.rdd.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", Environment.aws_access_key())
-        df.rdd.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", Environment.aws_secret_key())
+        val df = spark.sqlContext.read.json(sourcePath)
+        df.registerTempTable("dataFrame")
 
-        df.toDF()
-          .write
-          .mode(SaveMode.Append)
-          .parquet("s3a://swap-bigdata/legacy/logdna.parquet")    //Write
-          //.parquet("s3a://swap-bigdata/legacy/")    //Write
-
-
-//        df.toDF()
-//          .write
-//          .mode(SaveMode.Append)
-//          .parquet(destPath)
+        spark.sql("SELECT _source._host as host, " +
+          "_source._logtype as logtype, " +
+          "_source._mac as mac, " +
+          "_source._file as file, " +
+          "_source._line as line, " +
+          "_source._ts as ts, " +
+          "_source._app as app," +
+          "_source._ip as ip," +
+          "_source._ipremote as ipremote," +
+          "_source.level as level," +
+          "_source._lid as lid," +
+          "_source._tag[0] as tag FROM dataFrame").toDF()
+          .write.mode(SaveMode.Append).parquet(Environment.get_AWS_ParquetDestinationFolder())
 
         processStatus = true
       }
     } catch {
       case e: Exception => {
-        throw e
-        //throw new LoadDataException("Retorno do Processamento.: ".concat(false.toString).concat(" \n\nProblema no enriquecimento dos dados puros... Detalhes:".concat(e.getMessage)))
+        throw new LoadDataException("Retorno do Processamento.: ".concat(false.toString).concat(" \n\nProblema no enriquecimento dos dados puros... Detalhes:".concat(e.getMessage)))
       }
     } finally {
 
@@ -102,7 +80,7 @@ object EnrichmentEngine {
         updateHistoryOfExecution(enrichmentFiles)
     }
 
-    false
+    processStatus
   }
 
   private def processInLocalStorage(path: String): Boolean = {
@@ -129,8 +107,7 @@ object EnrichmentEngine {
       }
     } catch {
       case e: Exception => {
-        throw e
-        //throw new LoadDataException("Retorno do Processamento.: ".concat(false.toString).concat(" \n\nProblema no enriquecimento dos dados puros... Detalhes:".concat(e.getMessage)))
+        throw new LoadDataException("Retorno do Processamento.: ".concat(false.toString).concat(" \n\nProblema no enriquecimento dos dados puros... Detalhes:".concat(e.getMessage)))
       }
     } finally {
 
