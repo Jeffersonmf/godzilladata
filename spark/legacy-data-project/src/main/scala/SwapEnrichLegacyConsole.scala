a44a292005b693c1103b50c8fe4d1b1954fef92f
Jefferson Marchetti Ferreira <jefferson@contaswap.com>
2020-01-27 18:26:10 -0300

    Change the MANIFEST file config

--- spark/legacy-data-project/src/main/scala/SwapEnrichLegacyConsole.scala
+++ spark/legacy-data-project/src/main/scala/SwapEnrichLegacyConsole.scala
@@ -1,6 +1,28 @@
-object SwapEnrichLegacyConsole extends App {
+package com.swap.datateam
 
-  override def main(args: Array[String]): Unit = {
+object SwapEnrichLegacyConsole {
+
+  def main(args: Array[String]): Unit = {
+
+    import scala.io.Source
+    import java.io.File
+    import java.io.PrintWriter
+
     println("Welcome to Scala Enrichment Console Application")
+
+    val writer = new PrintWriter(new File("Write.txt"))
+
+    var x = ""
+
+    if (args.length == 0) {
+      x = "Nenhum valor foi enviado..."
+    } else {
+      x = args(0)
+    }
+
+    writer.write("Welcome to Scala Enrichment Console Application \n Arguments: " + x.toString)
+    writer.close()
+
+    Source.fromFile("Write.txt").foreach { x => print(x) }
   }
 }
