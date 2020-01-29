815790ffe876bd007173694d22e9d589891425ce
Jefferson Marchetti Ferreira <jefferson@contaswap.com>
2020-01-29 18:29:39 -0300

    Introduction to document of practices and politics of data

--- spark/legacy-data-project/src/main/scala/SwapEnrichLegacyConsole.scala
+++ spark/legacy-data-project/src/main/scala/SwapEnrichLegacyConsole.scala
@@ -1,28 +1,55 @@
 package com.swap.datateam
 
-object SwapEnrichLegacyConsole {
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.{Encoders, SaveMode, SparkSession}
+import org.joda.time.DateTime
 
-  def main(args: Array[String]): Unit = {
+object SwapEnrichLegacyConsole extends App {
+
+  final override def main(args: Array[String]): Unit = {
+    testSparkSubmit("main()")
+  }
+
+  def run(): Unit = {
+    testSparkSubmit("run()")
+  }
+
+  final def start(): String = {
+    try {
+      testSparkSubmit("start()")
+      "Done..."
+    } catch {
+      case e: Exception =>
+        testSparkSubmit("start() com exceção")
+        "Done with Error..."
+    }
+  }
+
+  final private def exec(): Unit = {
+    testSparkSubmit("exec()")
+  }
+
+  private def testSparkSubmit(origem: String) = {
 
     import scala.io.Source
     import java.io.File
     import java.io.PrintWriter
 
-    println("Welcome to Scala Enrichment Console Application")
+    println("Processando...")
 
-    val writer = new PrintWriter(new File("Write.txt"))
+    val writer = new PrintWriter(new File("/home/jefferson/spark-teste/Write.txt"))
 
     var x = ""
 
-    if (args.length == 0) {
+    if (null == args || args.length == 0) {
       x = "Nenhum valor foi enviado..."
     } else {
       x = args(0)
     }
 
-    writer.write("Welcome to Scala Enrichment Console Application \n Arguments: " + x.toString)
+    writer.write(s"Esse foi criado através do execução da função: ${origem} \n Arguments: " + x.toString)
     writer.close()
 
-    Source.fromFile("Write.txt").foreach { x => print(x) }
+    Source.fromFile("/home/jefferson/spark-teste/Write.txt").foreach { x => print(x) }
   }
 }
